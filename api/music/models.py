2d3e219bfacbcaf8b086d664b2895e46455703f6
Romain Brunie <romainbrunie@MacBook-Pro-de-Romain.local>
2017-06-07 19:44:20 +0200

    Modification models

--- api/music/models.py
+++ api/music/models.py
@@ -13,20 +13,23 @@ class Album(models.Model):
 		return '%s' % (self.name)
 
 class Video(models.Model):
-	url = models.CharField(max_length=200)
+	url = models.CharField(max_length=200, unique= True)
 	title = models.CharField(max_length=100)
-	song = models.OneToOneField(Song, null=True, blank=True)
+	song = models.OneToOneField("Song", null=True, blank=True)
 	def __str__(self):
 		return '%s' % (self.title)
-
+	def save(self, *args, **kwargs):
+		# acces api youtube
+		self.title = 
+		super(Video, self).save(*args, **kwargs)
 
 class Song(models.Model):
 	title = models.CharField(max_length=100)
 	album = models.ForeignKey(Album, on_delete=models.SET_NULL, null=True, blank=True)
 	artist = models.ManyToManyField(Artist)
-	
+	url = models.CharField(max_length=200, null=True, blank=True)
 	def __str__(self):
 		return '%s' % (self.title)
-
-	def __save__(self):
-
+	def save(self, *args, **kwargs):
+		Video.objects.get_or_create(url='http://www.google.fr', song=self)
+		super(Song, self).save(*args, **kwargs)
