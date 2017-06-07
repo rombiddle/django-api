488f9db5ff5eb81c6fd3dd8532ca9d170b1608e2
Salomé <salome.russier@gmail.com>
2017-06-07 19:56:07 +0200

    modif views

--- api/music/views.py
+++ api/music/views.py
@@ -28,7 +28,6 @@ def artist_list(request):
         return HttpResponse(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
-
 @csrf_exempt
 def artist_detail(request, pk):
     try:
@@ -55,6 +54,172 @@ def artist_detail(request, pk):
 
     elif request.method == 'DELETE':
         artist.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+    else:
+        return HttpResponse(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+@csrf_exempt
+def album_list(request):
+    if request.method == 'GET':
+        albums = Album.objects.all()
+        serializer = AlbumSerializer(albums, many=True)
+        return JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
+
+    elif request.method == 'POST':
+        try:
+            data = JSONParser.parse(request)
+        except ParseError:
+            return HttpResponse(status=400)
+        serializer = AlbumSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return JsonResponse(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return HttpResponse(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+@csrf_exempt
+def album_detail(request, pk):
+    try:
+        album = Album.objects.get(pk=pk)
+    except Album.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ArtistSerializer(album)
+        return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'PUT':
+        try:
+            data = JSONParser().parse(request)
+        except ParseError:
+            return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = ArtistSerializer(album, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        album.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+    else:
+        return HttpResponse(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+
+@csrf_exempt
+def song_list(request):
+    if request.method == 'GET':
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
+
+    elif request.method == 'POST':
+        try:
+            data = JSONParser.parse(request)
+        except ParseError:
+            return HttpResponse(status=400)
+        serializer = SongSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return JsonResponse(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return HttpResponse(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+@csrf_exempt
+def song_detail(request, pk):
+    try:
+        song = Song.objects.get(pk=pk)
+    except Song.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = SongSerializer(song)
+        return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'PUT':
+        try:
+            data = JSONParser().parse(request)
+        except ParseError:
+            return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = SongSerializer(song, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        song.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+    else:
+        return HttpResponse(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+
+
+
+@csrf_exempt
+def video_list(request):
+    if request.method == 'GET':
+        videos = Video.objects.all()
+        serializer = VideoSerializer(videos, many=True)
+        return JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
+
+    elif request.method == 'POST':
+        try:
+            data = JSONParser.parse(request)
+        except ParseError:
+            return HttpResponse(status=400)
+        serializer = VideoSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return JsonResponse(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return HttpResponse(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+@csrf_exempt
+def video_detail(request, pk):
+    try:
+        video = Video.objects.get(pk=pk)
+    except Video.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = SongSerializer(video)
+        return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'PUT':
+        try:
+            data = JSONParser().parse(request)
+        except ParseError:
+            return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = VideoSerializer(video, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        video.delete()
         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
 
     else:
